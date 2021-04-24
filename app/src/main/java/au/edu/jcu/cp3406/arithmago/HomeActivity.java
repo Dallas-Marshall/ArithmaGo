e3280b11c682f9a90169aa7b8a9de8de62d1db8d
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-04-24 12:36:34 +1000

    Add intent and listener to move to LeaderboardActivity

--- app/src/main/java/au/edu/jcu/cp3406/arithmago/HomeActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/arithmago/HomeActivity.java
@@ -2,7 +2,9 @@ package au.edu.jcu.cp3406.arithmago;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    public void leaderboardSelected(View buttonClicked) {
+        Intent intent = new Intent(this, LeaderboardActivity.class);
+        startActivity(intent);
     }
 }
